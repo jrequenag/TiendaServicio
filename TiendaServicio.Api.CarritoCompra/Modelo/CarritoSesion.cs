82e3557bbf96ec6bea9e2a4f826c6352b3fed98d
Jacobo Requena <jacoborequena@outlook.com>
2022-04-17 09:59:27 -0400

    ADD

--- TiendaServicio.Api.CarritoCompra/Modelo/CarritoSesion.cs
+++ TiendaServicio.Api.CarritoCompra/Modelo/CarritoSesion.cs
@@ -8,8 +8,6 @@ namespace TiendaServicio.Api.CarritoCompra.Modelo
 {
     public class CarritoSesion
     {
-        //internal object request;
-
         public int CarritoSesionId { get; set; }
         public DateTime? FechaCreacion { get; set; }
         public ICollection<CarritoSesionDetalle> ListaDetalle { get; set; } 
